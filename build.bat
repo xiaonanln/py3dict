6f121088f37910c1d4467472cd20989e9a059db2
seis <xiaonanln@gmail.com>
2018-04-29 00:43:52 +0800

    buildign

--- build.bat
+++ build.bat
@@ -1,4 +1,2 @@
 set VS90COMNTOOLS=%VS110COMNTOOLS%
-python setup.py build
-copy build\lib.win-amd64-2.7\*.pyd .\*
-@pause
+python setup.py build && copy build\lib.win-amd64-2.7\*.pyd .\*
---
+++ test_dictv1.py
@@ -0,0 +1,8 @@
+from dictv1 import dictv1
+d = dictv1()
+d.say_hello("dictv1")
+
+print len(d)
+print d['s']
+d[0] = 1
+
